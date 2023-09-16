39f477806e8193652dcba6fdb2e5562d2794c866
Albie Snyder <albie@snarton.com>
2023-09-16 15:00:46 -0400

    Added local test and sanatized input

--- Script01/ping_test.py
+++ Script01/ping_test.py
@@ -1,5 +1,5 @@
 #!/bin/python
-import os
+import subprocess
 
 
 options = "1. Display the default gateway \n2. Test Local Connectivity \n3. Test Remote Connectivity \n4. Test DNS Resolution \n5. Exit"
@@ -7,9 +7,18 @@ options = "1. Display the default gateway \n2. Test Local Connectivity \n3. Test
 userInput = 0
 
 while (userInput != 5):
-    userInput = int(input("What would you like to do? \n" + options + "\n"))
-
-    if userInput == 1:
-        gateway = os.system("ip r | head -1").split()[2]
-        print("Your default gateway is: " + str(gateway))
-
+	userTestInput =  input("What would you like to do? \n" + options + "\n")
+	try:
+		userInput = int(userTestInput)
+	except:
+		print("You have to choose one of the options!")
+		userInput = 0
+	if userInput == 1:
+        	gateway = subprocess.check_output("ip r | head -1 | cut -d ' ' -f 3", shell=True)
+        	print("Your default gateway is: " + str(gateway)[2:-3])
+	elif userInput == 2:
+		try:
+			subprocess.check_output("ping 0 -c 2", shell = True, stderr = subprocess.PIPE)
+			print("The test passed")
+		except subprocess.CalledProcessError as e:	
+			print("The test failed")
