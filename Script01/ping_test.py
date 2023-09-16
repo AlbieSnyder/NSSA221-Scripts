4cde6ff8b70fd9974ec232e75832775258b03511
Albie Snyder <albie@snarton.com>
2023-09-16 14:20:26 -0400

    ping_test.py

---
+++ Script01/ping_test.py
@@ -0,0 +1,15 @@
+#!/bin/python
+import os
+
+
+options = "1. Display the default gateway \n2. Test Local Connectivity \n3. Test Remote Connectivity \n4. Test DNS Resolution \n5. Exit"
+
+userInput = 0
+
+while (userInput != 5):
+    userInput = int(input("What would you like to do? \n" + options + "\n"))
+
+    if userInput == 1:
+        gateway = os.system("ip r | head -1 | cut -d " " -f 3")
+        print("Your default gateway is: " + gateway)
+
