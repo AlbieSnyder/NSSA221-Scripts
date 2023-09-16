8394dedf394d0941d197556bf65929ed59dd4f15
Albie Snyder <albie@snarton.com>
2023-09-16 15:04:00 -0400

    Added tests 3 and 4

--- Script01/ping_test.py
+++ Script01/ping_test.py
@@ -15,10 +15,22 @@ while (userInput != 5):
 		userInput = 0
 	if userInput == 1:
         	gateway = subprocess.check_output("ip r | head -1 | cut -d ' ' -f 3", shell=True)
-        	print("Your default gateway is: " + str(gateway)[2:-3])
+            print("Your default gateway is: " + str(gateway)[2:-3])
 	elif userInput == 2:
 		try:
 			subprocess.check_output("ping 0 -c 2", shell = True, stderr = subprocess.PIPE)
+			print("The test passed")
+		except subprocess.CalledProcessError as e:	
+			print("The test failed")
+    elif userInput == 3:
+		try:
+			subprocess.check_output("ping 8.8.8.8 -c 2", shell = True, stderr = subprocess.PIPE)
+			print("The test passed")
+		except subprocess.CalledProcessError as e:	
+			print("The test failed")
+	elif userInput == 4:
+		try:
+			subprocess.check_output("ping google.com -c 2", shell = True, stderr = subprocess.PIPE)
 			print("The test passed")
 		except subprocess.CalledProcessError as e:	
 			print("The test failed")
